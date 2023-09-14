4f10aa59468217cabd5bba2507a7e7f528d2166d
cola0817 <cola0817jz@gmail.com>
2023-09-14 22:14:06 +0800

    xxj job

---
+++ SpringBoot/Sb2_Template/backend/src/test/java/edu/jzxy/cbq/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jzxy.cbq.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
