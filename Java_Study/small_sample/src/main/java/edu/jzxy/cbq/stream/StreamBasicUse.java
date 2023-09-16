588581d78f93e6e8bb828320664d23834d4b7796
cola0817 <cola0817jz@gmail.com>
2023-09-16 10:23:51 +0800

    .filter(Objects::nonNull) 提高程序健壮性

--- Java_Study/small_sample/src/main/java/edu/jzxy/cbq/stream/StreamBasicUse.java
+++ Java_Study/small_sample/src/main/java/edu/jzxy/cbq/stream/StreamBasicUse.java
@@ -13,25 +13,28 @@ public class StreamBasicUse {
 
     /**
      * stream 过滤
+     *
      * @param userBeanList userBeanList
      * @return 根据条件过滤后的 list
      */
-    public List<UserBean> filterByStream(List<UserBean> userBeanList){
+    public List<UserBean> filterByStream(List<UserBean> userBeanList) {
         return
             userBeanList.stream()
-            .filter(userBean -> userBean.getAge() >20 )
+                .filter(Objects::nonNull)
+                .filter(userBean -> userBean.getAge() > 20)
                 .collect(Collectors.toList());
     }
 
     /**
      * 根据 forEach 过滤
+     *
      * @param userBeanList userBeanList
      * @return 根据条件过滤后的 list
      */
-    public List<UserBean> filterByForEach(List<UserBean> userBeanList){
+    public List<UserBean> filterByForEach(List<UserBean> userBeanList) {
         List<UserBean> newUserBeanList = new ArrayList<>();
         userBeanList.forEach(userBean -> {
-            if (userBean.getAge() > 20){
+            if (userBean.getAge() > 20) {
                 newUserBeanList.add(userBean);
             }
         });
@@ -41,16 +44,17 @@ public class StreamBasicUse {
 
     /**
      * 根据 Iterator 过滤
+     *
      * @param userBeanList userBeanList
      * @return 根据条件过滤后的 list
      */
-    public List<UserBean> filterByIterator(List<UserBean> userBeanList){
+    public List<UserBean> filterByIterator(List<UserBean> userBeanList) {
         List<UserBean> newUserBeanList = new ArrayList<>();
 
         Iterator<UserBean> iterator = userBeanList.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             UserBean userBean = iterator.next();
-            if (userBean.getAge() > 20){
+            if (userBean.getAge() > 20) {
                 newUserBeanList.add(userBean);
             }
         }
@@ -60,18 +64,20 @@ public class StreamBasicUse {
 
     /**
      * 比较 compare
+     *
      * @param userBeanList userBeanList
      * @return 比较后的 list
      */
-    public List<UserBean> sortedByCompare(List<UserBean> userBeanList){
-        return userBeanList.stream().sorted(new Comparator<UserBean>() {
-            @Override
-            public int compare(UserBean o1, UserBean o2) {
-                return o2.getAge() - o1.getAge();
-            }
-        }).collect(Collectors.toList());
+    public List<UserBean> sortedByCompare(List<UserBean> userBeanList) {
+        return userBeanList.stream()
+            .filter(Objects::nonNull)
+            .sorted(new Comparator<UserBean>() {
+                @Override
+                public int compare(UserBean o1, UserBean o2) {
+                    return o2.getAge() - o1.getAge();
+                }
+            }).collect(Collectors.toList());
     }
-
 
 
 }
