a92f9f5ed54b7fa9f288096e970f3bca4dc12048
cola0817 <cola0817jz@gmail.com>
2023-09-16 10:33:58 +0800

    Java_study book init commit

---
+++ Java_Study/book/src/main/java/edu/jzxy/cbq/chapter_14/init.java
@@ -0,0 +1,11 @@
+package edu.jzxy.cbq.chapter_14;
+
+/**
+ * @author Cola0817
+ * @name init
+ * @date 2023/9/16 10:31
+ * @since 1.0.0
+ */
+public class init {
+
+}
---
+++ Java_Study/book/src/main/java/edu/jzxy/cbq/chapter_15/init.java
@@ -0,0 +1,11 @@
+package edu.jzxy.cbq.chapter_15;
+
+/**
+ * @author Cola0817
+ * @name init
+ * @date 2023/9/16 10:31
+ * @since 1.0.0
+ */
+public class init {
+
+}
