1cd1b0d110f2a5cdaf1d80d89b257bfb775f5c8a
cola0817 <cola0817jz@gmail.com>
2023-09-16 13:06:02 +0800

    Java VarVariable

---
+++ Java_Study/book/src/main/java/edu/jzxy/cbq/chapter_02/VarVariable.java
@@ -0,0 +1,23 @@
+package edu.jzxy.cbq.chapter_02;
+
+/**
+ * @author Cola0817
+ * @name VarVariable
+ * @date 2023/9/16 12:58
+ * @since 1.0.0
+ */
+public class VarVariable {
+    public static void main(String[] args) {
+        var int_01 = 10;
+        var double_01 = 10.0;
+        var char_01 = 'A';
+        var char_02 = (char) 65;
+        var String_01 = "Cola";
+
+        System.out.println(int_01);
+        System.out.println(double_01);
+        System.out.println(char_01);
+        System.out.println(char_02);
+        System.out.println(String_01);
+    }
+}
